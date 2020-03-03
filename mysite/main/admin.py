aa1284c9cd4da3d50a7bfa5c7fa52fc95bae1f36
funkyxive <andreasnordenhof@gmail.com>
2020-03-03 11:26:01 +0100

    added forms.py and model work including work on extending the user model

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Names
+#from .models import Names
 # Register your models here.
-admin.site.register(Names)
+#admin.site.register(Names)
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,15 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
-class Names(models.Model):
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-class Users(models.Model):
-    username = models.CharField(max_length=50)
-    Password = models.CharField(max_length=50)
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class UserTypes(models.Model):
