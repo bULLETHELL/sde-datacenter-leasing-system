78c682403222603314796b2db11305dbe6c38977
funkyxive <andreasnordenhof@gmail.com>
2020-05-27 10:52:42 +0200

    random stuff

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -4,17 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "first_name", "last_name", "UserType", "password1", "password2")
-
-    def save(self, commit=True):
-        user = super(NewUserForm, self).save(commit=False)
-        user.UserType = self.cleaned_data["UserType"]
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
+# class NewUserForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
+#
+#     class Meta:
+#         model = User
+#         fields = ("username", "email", "first_name", "last_name", "password1", "password2")
+#
+#     def save(self, commit=True):
+#         user = super(NewUserForm, self).save(commit=False)
+#         user.UserType = self.cleaned_data["UserType"]
+#         user.email = self.cleaned_data["email"]
+#         if commit:
+#
+#             user.save()
+#         return user
